7f35247710e677a37016834f11d162bcda14ed4e
Alexandr Ovsyannikov <alexov2004@gmail.com>
2022-09-07 22:56:28 +0400

    models.py ready

--- microibm/communicate_to_db/models.py
+++ microibm/communicate_to_db/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Responces(models.Model):
+    name = models.TextField(null=False)
+    email = models.TextField()
+    comment = models.TextField(null=False)
+    createdAt = models.DateTimeField()
